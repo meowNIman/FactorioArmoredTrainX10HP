ef055617f34393d43790fef4857e0852ca224d67
meowNIman <nikita.mezjuev@yandex.ru>
2023-07-02 11:23:43 +0300

    Update flamethrower-wagon-mk1.lua
    flame wagon x10

--- assets/flamethrower-wagon-mk1/flamethrower-wagon-mk1.lua
+++ assets/flamethrower-wagon-mk1/flamethrower-wagon-mk1.lua
@@ -56,7 +56,7 @@ l_flamethrower_platform_mk1.minable =
 	mining_time = 0.5, 
 	result = "flamethrower-wagon-mk1"		
 }
-l_flamethrower_platform_mk1.max_health = 1000					-- Same as turret hp (Critical component or will be destroyed using old script)	
+l_flamethrower_platform_mk1.max_health = 10000					-- Same as turret hp (Critical component or will be destroyed using old script)	
 l_flamethrower_platform_mk1.weight = 2000
 l_flamethrower_platform_mk1.energy_per_hit_point = 5
 -- Animation
